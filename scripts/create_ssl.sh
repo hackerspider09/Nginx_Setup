d3bb18ad030a0501ff88196affaa6f0848fac35b
prasad <hackerspider09@gmail.com>
2024-10-09 18:14:27 +0530

    nginx added

--- scripts/create_ssl.sh
+++ scripts/create_ssl.sh
@@ -4,19 +4,19 @@
 # Author: Prasad
 # Date: 09/10/2024
 #
-# This script use to create SSL/TLS certificate
+# This script is used to create SSL/TLS certificates
 #
-# Version : v1
+# Version: v1
 ######################################
 
 DOMAIN="test.credenz.co.in"
 EMAIL="prasadkhatake20@gmail.com"  
 
 CERT_PATH="/etc/nginx/certs"
-CERT_FULLCHAIN="$CERT_PATH/live/$DOMAIN/fullchain.pem"
-CERT_PRIVKEY="$CERT_PATH/live/$DOMAIN/privkey.pem"
+CERT_FULLCHAIN="$CERT_PATH/fullchain.pem"
+CERT_PRIVKEY="$CERT_PATH/privkey.pem"
 
-# Check if certificate already exists
+# Check if the certificate already exists
 if [ -f "$CERT_FULLCHAIN" ] && [ -f "$CERT_PRIVKEY" ]; then
     echo "SSL certificates already exist for $DOMAIN."
 else
@@ -24,12 +24,12 @@ else
 
     # Create SSL certificates using Certbot
     certbot certonly --standalone --non-interactive --agree-tos \
-        --email $EMAIL -d $DOMAIN --config-dir /etc/nginx/certs --work-dir /etc/nginx/certs --logs-dir /etc/nginx/certs
+        --email $EMAIL -d $DOMAIN --config-dir /etc/nginx/certs \
+        --work-dir /etc/nginx/certs --logs-dir /etc/nginx/certs
 
     # Copy the certs to the right location
-    cp /etc/nginx/certs/live/$DOMAIN/fullchain.pem /etc/nginx/certs/fullchain.pem
-    cp /etc/nginx/certs/live/$DOMAIN/privkey.pem /etc/nginx/certs/privkey.pem
+    cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem "$CERT_FULLCHAIN"
+    cp /etc/letsencrypt/live/$DOMAIN/privkey.pem "$CERT_PRIVKEY"
 
-    echo "SSL certificates have been created and stored in /etc/nginx/certs"
+    echo "SSL certificates have been created and stored in $CERT_PATH."
 fi
-
